0a49b9fef7c941e7d97a5af5a81316217c861d66
DreynPlay <dreynplay@gmail.com>
2026-02-05 23:50:29 +0500

    manifest and icons changed

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,13 +12,19 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#1f1f23",
     icons: [
       {
-        src: "/apple-icon.png",
+        src: "/icon-192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon.png",
+        src: "/icon-512.png",
         sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/apple-icon.png",
+        sizes: "180x180",
         type: "image/png",
       },
     ],
