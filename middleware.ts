0a7113c3f37acf3e139f0f445560acc29d1f2080
DreynPlay <dreynplay@gmail.com>
2026-03-11 23:43:07 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,8 @@
-import { withAuth } from "next-auth/middleware";
-
-export default withAuth({
-  pages: {
-    signIn: "/api/auth/signin",
-  },
-});
+// export default withAuth({
+//   pages: {
+//     signIn: "/api/auth/signin",
+//   },
+// });
 
 export const config = {
   matcher: [
