b658dab228be5e7f61a3340065fc3eb6e79c74ac
DreynPlay <dreynplay@gmail.com>
2026-03-11 23:43:45 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,16 @@
-// export default withAuth({
-//   pages: {
-//     signIn: "/api/auth/signin",
-//   },
-// });
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/api/auth/signin",
+  },
+});
 
 export const config = {
   matcher: [
     // Защищаем ВСЁ приложение, кроме:
     // - /api/auth/*
     // - статических файлов, картинок, манифестов PWA
-    "/((?!api/auth|_next/static|_next/image|favicon.ico|sw.js|workbox-.*|manifest.json|icon-.*).*)",
+    "/((?!api/auth|api/zitadel|_next/static|_next/image|favicon.ico|sw.js|workbox-.*|manifest.json|icon-.*).*)",
   ],
 };
