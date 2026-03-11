88a601b76d12db2c2cf34b4275efac95a360c7c3
DreynPlay <dreynplay@gmail.com>
2026-03-11 23:39:01 +0500

    Update route.ts

--- app/api/zitadel/actions/log/route.ts
+++ app/api/zitadel/actions/log/route.ts
@@ -3,9 +3,13 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const body = await req.json();
 
+  console.log("🔥 HOT BODY FREE:\n", body);
+
   // В V2 данные приходят в теле запроса (Request)
   // Мы ищем структуру пользователя, которую Zitadel передает в метод создания
   const user = body.user;
+
+  console.log("🔥 HOT USER FREE:\n", user);
 
   if (
     user?.human?.profile &&
