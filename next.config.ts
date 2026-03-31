46dbd39d0907324d45b6ceaf845ef4845ea58bf3
DreynPlay <dreynplay@gmail.com>
2026-03-31 21:28:30 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -56,7 +56,8 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'",
+            value:
+              "default-src 'self'; connect-src 'self' https://id-medincident.ulbwa.bombomeow.ru; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-eval' 'unsafe-inline';",
           },
         ],
       },
